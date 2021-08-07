3378748c21c96ee00ba89c24e39a9b93a0256a3f
hirocook55-hub <83267875+hirocook55-hub@users.noreply.github.com>
2021-08-07 14:13:06 +0900

    Add files via upload

---
+++ my-service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener('install',(Event=>{
+    console.log('service worker install ...');
+}));
+
+self.addEventListener('activate',(Event)=>{
+    console.info('activate',Event);
+});
