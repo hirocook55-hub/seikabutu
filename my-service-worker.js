edba22928dc5dc42838fc581605c623e221ee4e5
hirocook55-hub <83267875+hirocook55-hub@users.noreply.github.com>
2021-08-07 15:46:17 +0900

    Update my-service-worker.js

--- my-service-worker.js
+++ my-service-worker.js
@@ -2,8 +2,8 @@
     'use strict';
 self.addEventListener('install',(Event=>{
     console.log('service worker install ...');
-}));
+});
 
 self.addEventListener('activate',(Event)=>{
     console.info('activate',Event);
-})
+});
