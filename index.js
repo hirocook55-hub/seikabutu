19dca219cde830cd818033742adbc6864f973910
hirocook55-hub <83267875+hirocook55-hub@users.noreply.github.com>
2021-08-07 15:37:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
     'use strict';
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/my-service-worker.js',{scope:'/'})
+    navigator.serviceWorker.register('/seikabutu/my-service-worker.js',{scope:'/'})
     .then(function(reg){
         console.log('登録に成功しました。Scopeは'+ reg.scope);
     }).catch(function(error){
